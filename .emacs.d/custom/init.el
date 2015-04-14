7b1ab880855b296c3b853d6b5a99a4b46314653b
Song Xie <me@xiesong.me>
2015-04-13 18:10:45 -0700

    [Emacs] Fix smart-split + customize powerline

--- .emacs.d/custom/init.el
+++ .emacs.d/custom/init.el
@@ -7,6 +7,81 @@
 (require 'moe-theme)
 (moe-dark)
 (powerline-moe-theme)
+(setq powerline-display-buffer-size nil)
+(setq-default mode-line-format
+              '("%e"
+                (:eval
+                 (let*
+                     ((active
+                       (powerline-selected-window-active))
+                      (mode-line
+                       (if active 'mode-line 'mode-line-inactive))
+                      (face1
+                       (if active 'powerline-active1 'powerline-inactive1))
+                      (face2
+                       (if active 'powerline-active2 'powerline-inactive2))
+                      (separator-left
+                       (intern
+                        (format "powerline-%s-%s"
+                                (powerline-current-separator)
+                                (car powerline-default-separator-dir))))
+                      (separator-right
+                       (intern
+                        (format "powerline-%s-%s"
+                                (powerline-current-separator)
+                                (cdr powerline-default-separator-dir))))
+                      (lhs
+                       (list
+                        (powerline-raw "%*" nil 'l)
+                        (when powerline-display-buffer-size
+                          (powerline-buffer-size nil 'l))
+                        (when powerline-display-mule-info
+                          (powerline-raw mode-line-mule-info nil 'l))
+                        (powerline-buffer-id nil 'l)
+                        (when
+                            (and
+                             (boundp 'which-func-mode)
+                             which-func-mode)
+                          (powerline-raw which-func-format nil 'l))
+                        (powerline-raw " ")
+                        (funcall separator-left mode-line face1)
+                        (when
+                            (boundp 'erc-modified-channels-object)
+                          (powerline-raw erc-modified-channels-object face1 'l))
+                        (powerline-major-mode face1 'l)
+                        (powerline-process face1)
+                        ;; (powerline-minor-modes face1 'l)
+                        (powerline-narrow face1 'l)
+                        (powerline-raw " " face1)
+                        (funcall separator-left face1 face2)
+                        ;; (powerline-vc face2 'r)
+                        (when
+                            (bound-and-true-p nyan-mode)
+                          (powerline-raw
+                           (list
+                            (nyan-create))
+                           face2 'l))))
+                      (rhs
+                       (list
+                        (powerline-raw global-mode-string face2 'r)
+                        (funcall separator-right face2 face1)
+                        (unless window-system
+                          (powerline-raw
+                           (char-to-string 57505)
+                           face1 'l))
+                        (powerline-raw "%4l" face1 'l)
+                        (powerline-raw ":" face1 'l)
+                        (powerline-raw "%3c" face1 'r)
+                        (funcall separator-right face1 mode-line)
+                        (powerline-raw " ")
+                        (powerline-raw "%6p" nil 'r)
+                        (when powerline-display-hud
+                          (powerline-hud face2 face1)))))
+                   (concat
+                    (powerline-render lhs)
+                    (powerline-fill face2
+                                    (powerline-width rhs))
+                    (powerline-render rhs))))))
 
 ; (desktop-save-mode 1)
 
@@ -26,10 +101,6 @@
 
 (defvar on_Terminal
   (string-match "nil" (prin1-to-string window-system)))
-
-;; color-theme
-(require 'color-theme-wombat)
-(color-theme-wombat)
 
 ;; scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
@@ -178,9 +249,6 @@ that was stored with ska-point-to-register."
 
 (setq line-number-mode t)
 (setq column-number-mode t)
-(setq display-time-24hr-format t)
-(setq display-time-day-and-date t)
-(display-time)
 
 (global-font-lock-mode t)
 
@@ -226,7 +294,7 @@ that was stored with ska-point-to-register."
     "If the given window can be split into multiple (fill-column)-column
          windows, do it."
     (when (> (window-width w) (* 2 (+ fill-column 1)))
-      (let ((w2 (split-window w (+ fill-column 2) t)))
+      (let ((w2 (split-window w (+ fill-column 1) t)))
 	(save-excursion
 	  (select-window w2)
 	  (switch-to-buffer (other-buffer (window-buffer w))))
